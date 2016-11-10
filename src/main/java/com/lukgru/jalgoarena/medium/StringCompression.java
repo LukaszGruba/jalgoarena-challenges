344e39171efdadb67fe7879350079b84025b71eb
LukaszGruba <lukgru@gmail.com>
2016-11-10 08:59:11 +0100

    init

---
+++ src/main/java/com/lukgru/jalgoarena/medium/StringCompression.java
@@ -0,0 +1,16 @@
+package com.lukgru.jalgoarena.medium;
+
+/**
+ * Implement a method compress to perform basic string compression using the counts of repeated characters.
+ If the compressed string would not become smaller than original string, your method should return original string.
+ Note: You can assume the string has only uppercase and lowercase letters (a-z).
+ Examples
+ "aabcccccaaa" -> "a2b1c5a3"
+ */
+public class StringCompression {
+
+    public String compress(String str) {
+        return null;
+    }
+
+}
