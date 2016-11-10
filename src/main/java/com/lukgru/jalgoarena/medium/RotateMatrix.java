1d365ba133f9d0b3f37d441280d5e205d4b9ab00
LukaszGruba <lukgru@gmail.com>
2016-11-10 18:24:41 +0100

    input added

--- src/main/java/com/lukgru/jalgoarena/medium/RotateMatrix.java
+++ src/main/java/com/lukgru/jalgoarena/medium/RotateMatrix.java
@@ -12,6 +12,17 @@ package com.lukgru.jalgoarena.medium;
  [4, 0, 6, 2],
  [5, 1, 7, 3],
  [6, 2, 8, 4]]
+
+
+ 0,
+
+ 1,2,
+ 5,6,
+
+ 1,2,3,4,
+ 5,6,7,8,
+ 9,0,1,2,
+ 3,4,5,6,
  */
 public class RotateMatrix {
 
