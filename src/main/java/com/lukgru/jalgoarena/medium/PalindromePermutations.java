62528d608de35a6b1a40c7eba40e7ba7a3a2d22e
LukaszGruba <lukgru@gmail.com>
2016-11-10 08:54:09 +0100

    init

---
+++ src/main/java/com/lukgru/jalgoarena/medium/PalindromePermutations.java
@@ -0,0 +1,15 @@
+package com.lukgru.jalgoarena.medium;
+
+/**
+ * Given a string, write a method isPermutationOfPalindrome to check if it is a permutation of a palindrome.
+ The palindrome does not need to be limited to just dictionary words (skip non-letter characters). Your solution should be case insensitive.
+ Examples
+ "Tact Coa" -> true (permutations: "taco cat", "acto tca", etc.)
+ */
+public class PalindromePermutations {
+
+    public boolean isPermutationOfPalindrome(String phrase) {
+        return false;
+    }
+
+}
