5a3622bc395a95080142e10fb19308a288bdc29a
LukaszGruba <lukgru@gmail.com>
2016-11-10 08:45:04 +0100

    init

---
+++ src/main/java/com/lukgru/jalgoarena/medium/CountPaths.java
@@ -0,0 +1,19 @@
+package com.lukgru.jalgoarena.medium;
+
+/**
+ * You're given a game board that has m x n squares on it, represented by an m x n array. Write a method countPaths that takes in m and n and returns the number of possible paths from the top left corner to the bottom right corner. Only down and right directions of movement are permitted.
+ Examples
+ countPaths(m = 2, n = 2) => 2
+
+ as on the following 2x2 Board, the two paths are A->C->D and A->B->D
+
+ A B
+ C D
+ */
+public class CountPaths {
+
+    public int countPaths(int m, int n) {
+        return -1;
+    }
+
+}
