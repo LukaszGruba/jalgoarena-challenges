3ffcefea3baf049f404136e75978e8dd777c9909
LukaszGruba <lukgru@gmail.com>
2016-11-10 08:48:58 +0100

    init

---
+++ src/main/java/com/lukgru/jalgoarena/medium/IsListCyclic.java
@@ -0,0 +1,20 @@
+package com.lukgru.jalgoarena.medium;
+
+import com.lukgru.jalgoarena.ListNode;
+
+/**
+ * Given a singly linked list, write a method isCyclic to check if the list has cycles.
+ * The space complexity can be O(n).
+ * If there is a cycle, return true otherwise return false.
+ * Empty lists should be considered non-cyclic.
+ *
+ * Examples
+ * 1->2->3->4->5->6->1 -> true
+ */
+public class IsListCyclic {
+
+    public boolean isCyclic(ListNode head) {
+        return false;
+    }
+
+}
