3a995110c076f0ddd47ee994f750605ff9984586
LukaszGruba <lukgru@gmail.com>
2016-11-12 10:31:52 +0100

    PalindromePermutations solution + tests

---
+++ src/test/java/com/lukgru/jalgoarena/medium/PalindromePermutationsTest.java
@@ -0,0 +1,109 @@
+package com.lukgru.jalgoarena.medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Łukasz on 2016-11-12.
+ */
+public class PalindromePermutationsTest {
+
+    private PalindromePermutations palindromePermutations = new PalindromePermutations();
+
+    @Test
+    public void givenNullReturnFalse() {
+        //given
+        String input = null;
+
+        //when
+        boolean result = palindromePermutations.isPermutationOfPalindrome(input);
+
+        //then
+        assertFalse(result);
+    }
+
+    @Test
+    public void givenSingleCharacterReturnTrue() {
+        //given
+        String input = "a";
+
+        //when
+        boolean result = palindromePermutations.isPermutationOfPalindrome(input);
+
+        //then
+        assertTrue(result);
+    }
+
+    @Test
+    public void givenEvenCharactersWithMoreThanOneUniqueShouldReturnFalse() {
+        //given
+        String input = "cxzwcxzwcxzwcxzwab";
+
+        //when
+        boolean result = palindromePermutations.isPermutationOfPalindrome(input);
+
+        //then
+        assertFalse(result);
+    }
+
+    @Test
+    public void givenOddCharactersWithOneUniqueShouldReturnTrue() {
+        //given
+        String input = "cxzwcxzwcxzwcxzwa";
+
+        //when
+        boolean result = palindromePermutations.isPermutationOfPalindrome(input);
+
+        //then
+        assertTrue(result);
+    }
+
+    @Test
+    public void shouldIgnoreLetterCase() {
+        //given
+        String input = "Aa";
+
+        //when
+        boolean result = palindromePermutations.isPermutationOfPalindrome(input);
+
+        //then
+        assertTrue(result);
+    }
+
+    @Test
+    public void shouldWorkForNonLetterCharacters() {
+        //given
+        String input = "Tact Coa&";
+
+        //when
+        boolean result = palindromePermutations.isPermutationOfPalindrome(input);
+
+        //then
+        assertFalse(result);
+    }
+
+    @Test
+    public void shouldIgnoreSpaces() {
+        //given
+        String input = "Tact Coa";
+
+        //when
+        boolean result = palindromePermutations.isPermutationOfPalindrome(input);
+
+        //then
+        assertTrue(result);
+    }
+
+    @Test
+    public void shouldWorkForSingleWord() {
+        //given
+        String input = "abcabcd";
+
+        //when
+        boolean result = palindromePermutations.isPermutationOfPalindrome(input);
+
+        //then
+        assertTrue(result);
+    }
+}
