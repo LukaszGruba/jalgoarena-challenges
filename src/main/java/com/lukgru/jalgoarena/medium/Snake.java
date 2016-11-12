2568b9aab9febd5ff3a66d91eb91eca7283b3838
LukaszGruba <lukgru@gmail.com>
2016-11-12 13:26:44 +0100

    Snake solution + tests

--- src/main/java/com/lukgru/jalgoarena/medium/Snake.java
+++ src/main/java/com/lukgru/jalgoarena/medium/Snake.java
@@ -9,29 +9,70 @@ package com.lukgru.jalgoarena.medium;
  {7, 8, 9}
 
  Output ArrayList:[1, 2, 3, 6, 9, 8, 7, 4, 5]
-
-
-
-
- 0,
-
- 1,2,3,4,
- 5,6,7,8,
- 9,0,1,2,
- 3,4,5,0,
-
- 1,0,
- 1,0,
-
- 1,2,3,4,5,6,
- 7,8,9,10,11,12,
- 13,14,15,16,17,18,
-
  */
 public class Snake {
 
     public int[] findSpiral(int[][] matrix) {
-        return null;
+        if (matrix == null) return null;
+
+        int m = matrix.length ;
+        int n = matrix[0].length;
+        int numberOfElements = m * n;
+
+        if (numberOfElements == 1) return new int[]{matrix[0][0]};
+
+        int[] result = new int[numberOfElements];
+        int resultIndex = 0;
+
+        int posM = 0;
+        int posN = -1;
+
+        int rightB = n - 1;
+        int leftB = 0;
+        int bottomB = m - 1;
+        int topB = 1;
+
+        int dir = 0;
+
+        while (resultIndex < numberOfElements) {
+            if (dir == 0) {
+                posN++;
+                result[resultIndex] = matrix[posM][posN];
+                resultIndex++;
+                if (posN == rightB) {
+                    dir = 1;
+                    rightB--;
+                }
+            }
+            else if (dir == 1) {
+                posM++;
+                result[resultIndex] = matrix[posM][posN];
+                resultIndex++;
+                if (posM == bottomB) {
+                    dir = 2;
+                    bottomB--;
+                }
+            }
+            else if (dir == 2) {
+                posN--;
+                result[resultIndex] = matrix[posM][posN];
+                resultIndex++;
+                if (posN == leftB) {
+                    dir = 3;
+                    leftB++;
+                }
+            }
+            else {
+                posM--;
+                result[resultIndex] = matrix[posM][posN];
+                resultIndex++;
+                if (posM == topB) {
+                    dir = 0;
+                    topB++;
+                }
+            }
+        }
+        return result;
     }
 
 }
