d0742232fccc8b2506dfff59c62849c799ad07e4
LukaszGruba <lukgru@gmail.com>
2016-11-14 23:35:20 +0100

    ReturnKthToLast solution + tests

--- src/main/java/com/lukgru/jalgoarena/medium/ReturnKthToLast.java
+++ src/main/java/com/lukgru/jalgoarena/medium/ReturnKthToLast.java
@@ -8,32 +8,43 @@ import com.lukgru.jalgoarena.ListNode;
  1->2->3->4->5->6, 3 -> 3
  Notes
  If index is absent, return 0.
-
-
- 12,1,2,2,3,3,4,4,3,1,    1
-10,1,2,2,3,3,4,4,3,6,    8
-13,1,1,1,2,3,4,3,3,    100
-101,1,2,2,3,3,4,4,3,6,1,10,1,2,2,3,3,4,4,3,6,1,10,1,2,2,3,3,4,4,3,6,1,10,1,2,2,3,3,4,4,3,6,1,10,1,2,2,3,3,4,4,3,6,1,10,1,2,2,3,3,4,4,3,6,1,10,1,2,2,3,3,4,4,3,6,1,10,1,2,2,3,3,4,4,3,6,1,10,1,2,2,3,3,4,4,3,6,1,10,1,2,2,3,3,4,4,3,6,1,10,1,2,2,3,3,4,4,3,6,1,10,1,2,2,3,3,4,4,3,6,1,10,1,2,2,3,3,4,4,3,6,1,10,1,2,2,3,3,4,4,3,6,1,10,1,2,2,3,3,4,4,3,6,1,10,1,2,2,3,3,4,4,3,6,1,10,1,2,2,3,3,4,4,3,6,1,10,1,2,2,3,3,4,4,3,6,1,10,1,2,2,3,3,4,4,3,6,1,10,1,2,2,3,3,4,4,3,6,1,    219
-11,1,2,2,3,3,4,4,3,6,    -1
-1,2,3,4,5,6,    3
-
  */
 public class ReturnKthToLast {
 
-    //FIXME: something is not working
+    public int kthToLast2(ListNode head, int k) {
+        if (k++ < 0) return 0;
+        int[] lastValues = new int[k];
+        int i = 0;
+        int counter = 0;
+        while (head != null) {
+            lastValues[i] = head.value;
+            counter++;
+            i = ++i % k;
+            head = head.next;
+        }
+        return counter != i ? lastValues[i] : 0;
+    }
+
     public int kthToLast(ListNode head, int k) {
         ListNode node = head;
         if (k < 0) return 0;
-        int[] lastValues = new int[k];
-        int i = 0;
-        boolean b = false;
-        while (head != null) {
-            lastValues[i] = head.value;
-            b = b || i + 1 == k;
-            i = (i + 1) % k;
-            head = head.next;
+        if (k == 0) return head.value;
+
+        int counter = 0;
+        while (node != null) {
+            node = node.next;
+            counter++;
         }
-        return b ? lastValues[i] : 0;
+
+        node = head;
+        int i = 0;
+        int index = counter - k - 1;
+        if (index < 0) return 0;
+        while (i < index) {
+            i++;
+            node = node.next;
+        }
+        return node.value;
     }
 
 }
