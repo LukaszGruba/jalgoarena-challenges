41e55681e23dd016810ab572ded54c62c0b6b6d2
LukaszGruba <lukgru@gmail.com>
2016-11-23 20:29:53 +0100

    tests

---
+++ src/main/java/com/lukgru/jalgoarena/easy/FirstNonRepeatedCharacter.java
@@ -0,0 +1,14 @@
+package com.lukgru.jalgoarena.easy;
+
+/**
+ * Write a method findFirstNonRepeatedChar that finds the first non-duplicate character in a string. Return null if no unique character is found.
+ Example
+ 'asdsdakz -> 'k'
+ */
+public class FirstNonRepeatedCharacter {
+
+    public String findFirstNonRepeatedChar(String str) {
+        return null;
+    }
+
+}
---
+++ src/test/java/com/lukgru/jalgoarena/easy/FirstNonRepeatedCharacterTest.java
@@ -0,0 +1,76 @@
+package com.lukgru.jalgoarena.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Łukasz on 2016-11-23.
+ */
+public class FirstNonRepeatedCharacterTest {
+
+    private FirstNonRepeatedCharacter firstNonRepeatedCharacter = new FirstNonRepeatedCharacter();
+
+    @Test
+    public void test1() {
+        //given
+        String input = null;
+
+        //when
+        String output = firstNonRepeatedCharacter.findFirstNonRepeatedChar(input);
+
+        //then
+        assertNull(output);
+    }
+
+    @Test
+    public void test2() {
+        //given
+        String input = "";
+
+        //when
+        String output = firstNonRepeatedCharacter.findFirstNonRepeatedChar(input);
+
+        //then
+        String expected = "";
+        assertEquals(expected, output);
+    }
+
+    @Test
+    public void test3() {
+        //given
+        String input = "asdsdakz";
+
+        //when
+        String output = firstNonRepeatedCharacter.findFirstNonRepeatedChar(input);
+
+        //then
+        String expected = "k";
+        assertEquals(expected, output);
+    }
+
+    @Test
+    public void test4() {
+        //given
+        String input = "asdsda";
+
+        //when
+        String output = firstNonRepeatedCharacter.findFirstNonRepeatedChar(input);
+
+        //then
+        assertNull(output);
+    }
+
+    @Test
+    public void test5() {
+        //given
+        String input = "asd";
+
+        //when
+        String output = firstNonRepeatedCharacter.findFirstNonRepeatedChar(input);
+
+        //then
+        String expected = "a";
+        assertEquals(expected, output);
+    }
+}
