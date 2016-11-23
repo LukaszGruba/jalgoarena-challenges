edc2acceba2ef47875a8bbf7480fc432dbd246bb
LukaszGruba <lukgru@gmail.com>
2016-11-23 16:48:23 +0100

    init

---
+++ src/main/java/com/lukgru/jalgoarena/easy/PreorderTraversal.java
@@ -0,0 +1,30 @@
+package com.lukgru.jalgoarena.easy;
+
+import com.lukgru.jalgoarena.TreeNode;
+
+/**
+ Given a binary tree, Write a method preorderTraversal to traverse the tree in the preorder manner. Return array of elements visited in preorder format.
+ Example
+ 1
+ / \
+ 2   3     ==> 1245367
+ / \ / \
+ 4  5 6  7
+
+ v:1l:v:2l:v:4l:nullr:nullr:v:5l:nullr:nullr:v:3l:v:6l:nullr:nullr:v:7l:nullr:null
+ null
+ v:1l:v:2l:v:4l:nullr:nullr:nullr:v:3l:nullr:v:7l:nullr:null
+ v:1l:nullr:null
+ */
+public class PreorderTraversal {
+
+    public class Solution {
+        /**
+         * @param root Root of binary tree
+         * @return  Preordered array of binary tree elements
+         */
+        public int[] preorderTraversal(TreeNode root) {
+            return null;
+        }
+    }
+}
