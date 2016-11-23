866743e975a55de35ef1e08bf3ca8e95d62a162c
LukaszGruba <lukgru@gmail.com>
2016-11-23 22:40:09 +0100

    PostorderTraversal  solution

---
+++ src/test/java/com/lukgru/jalgoarena/easy/PostorderTraversalTest.java
@@ -0,0 +1,69 @@
+package com.lukgru.jalgoarena.easy;
+
+import com.lukgru.jalgoarena.TreeNode;
+import com.lukgru.jalgoarena.TreeNodeBuilder;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Łukasz on 2016-11-23.
+ */
+public class PostorderTraversalTest {
+
+    private PostorderTraversal inorderTraversal = new PostorderTraversal();
+
+    private TreeNodeBuilder treeNodeBuilder = new TreeNodeBuilder();
+
+    @Test
+    public void test1() {
+        //given
+        TreeNode root = null;
+
+        //when
+        int[] traversal = inorderTraversal.postorderTraversal(root);
+
+        //then
+        assertNull(traversal);
+    }
+
+    @Test
+    public void test2() {
+        //given
+        TreeNode root = new TreeNode(1);
+
+        //when
+        int[] traversal = inorderTraversal.postorderTraversal(root);
+
+        //then
+        int[] expected = {1};
+        assertArrayEquals(expected, traversal);
+    }
+
+    @Test
+    public void test3() {
+        //given
+        TreeNode root = treeNodeBuilder.treeNode("1,2,3,4,5,6,7");
+
+        //when
+        int[] traversal = inorderTraversal.postorderTraversal(root);
+
+        //then
+        int[] expected = {4,2,5,1,6,3,7};
+        assertArrayEquals(expected, traversal);
+    }
+
+    @Test
+    public void test4() {
+        //given
+        TreeNode root = treeNodeBuilder.treeNode("1,2,3,4,*,*,7");
+
+        //when
+        int[] traversal = inorderTraversal.postorderTraversal(root);
+
+        //then
+        int[] expected = {4,2,1,3,7};
+        assertArrayEquals(expected, traversal);
+    }
+
+}
